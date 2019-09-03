538a6bfef4f7e715ad1e451f6a27c5819d33bc34
DisMirror <510009842@qq.com>
2019-09-03 22:09:37 +0800

    redis工具类更新

--- src/main/java/ggze/tool/RedisUtils.java
+++ src/main/java/ggze/tool/RedisUtils.java
@@ -2,6 +2,7 @@ package ggze.tool;
 
 import redis.clients.jedis.Jedis;
 
+import java.io.*;
 import java.util.List;
 
 public class RedisUtils<T> {
@@ -26,6 +27,55 @@ public class RedisUtils<T> {
     }
 
     public int setList(String key, T values){
-        return 0;
+        try {
+            RedisUtils.conntion.set(key.getBytes(),serialize(values));
+            return 0;
+        }catch (Exception e){
+            return 1;
+        }
+    }
+
+    public T getList(String key, T values){
+        try {
+            byte[] re = RedisUtils.conntion.get(key.getBytes());
+            T obj=(T)unserizlize(re);
+            return obj;
+        }catch (Exception e){
+            return null;
+        }
+    }
+
+    //序列化
+    public static byte [] serialize(Object obj){
+        ObjectOutputStream obi=null;
+        ByteArrayOutputStream bai=null;
+        try {
+            bai=new ByteArrayOutputStream();
+            obi=new ObjectOutputStream(bai);
+            obi.writeObject(obj);
+            byte[] byt=bai.toByteArray();
+            return byt;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    //反序列化
+    public static Object unserizlize(byte[] byt){
+        ObjectInputStream oii=null;
+        ByteArrayInputStream bis=null;
+        bis=new ByteArrayInputStream(byt);
+        try {
+            oii=new ObjectInputStream(bis);
+            Object obj=oii.readObject();
+            return obj;
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+
+
+        return null;
     }
 }
